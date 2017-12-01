f191c3a1fd9e393aabeb6871f280509804127cba
Miftahussalam <miftahussalam08@gmail.com>
2017-12-01 22:31:46 +0700

    typo web

--- ms_report_smove_xls_cst/__openerp__.py
+++ ms_report_smove_xls_cst/__openerp__.py
@@ -2,7 +2,7 @@
     "name": "Report Stock Movement .xls",
     "version": "1.0",
     "author": "Miftahussalam",
-    "website": "http://miftahsalam.worpress.com",
+    "website": "http://miftahsalam.wordpress.com",
     "category": "New Module",
     "description": "Report Stock Movement .xls",
     "depends": ["ms_base_cst","report_xls","stock"],
